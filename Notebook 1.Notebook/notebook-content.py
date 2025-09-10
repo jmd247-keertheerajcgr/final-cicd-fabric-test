fa83356532920982d3c90ea0fce6e20e56f31e14
jmd247-keertheerajcgr <147801908+jmd247-keertheerajcgr@users.noreply.github.com>
2025-09-10 23:54:08 +0530

    Committing 2 items from workspace b8284fa4-3266-4b97-84a6-04e6808c474d

---
+++ Notebook 1.Notebook/notebook-content.py
@@ -0,0 +1,34 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {
+# META     "lakehouse": {
+# META       "default_lakehouse": "3e3f0aeb-7997-4f4b-b1f5-05437b03ee3c",
+# META       "default_lakehouse_name": "lk_dev",
+# META       "default_lakehouse_workspace_id": "b8284fa4-3266-4b97-84a6-04e6808c474d",
+# META       "known_lakehouses": [
+# META         {
+# META           "id": "3e3f0aeb-7997-4f4b-b1f5-05437b03ee3c"
+# META         }
+# META       ]
+# META     }
+# META   }
+# META }
+
+# CELL ********************
+
+# Welcome to your new notebook
+# Type here in the cell editor to add code!
+
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
